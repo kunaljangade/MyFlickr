afcad8ba67578c88024089e4d6f50be9af69dfd9
kunaljangade <jangade.kunal@gmail.com>
2015-05-08 20:10:07 -0700

    MyFlickr

---
+++ app/src/main/java/com/example/pc/myflickr/Constants.java
@@ -0,0 +1,20 @@
+package com.example.pc.myflickr;
+
+/**
+ * Created by mark on 4/29/15.
+ */
+public class Constants {
+    public static final int NUM_PHOTOS = 10;
+    public static final String TAG = "my code";
+    public static final String API_KEY = "16a1aff53c841654b90bb5a2bf46dcf6";
+    public static final String SECRET = "1ac5e34aea526205";
+    //    public static final String LATITUDE = "34.0672280";
+//    public static final String LONGITUDE = "-118.1667410";
+    public static final String RADIUS = "1";
+
+    public static final String LATITUDE = "40.7127837";
+    public static final String LONGITUDE = "-74.00594130000002";
+
+
+
+}
